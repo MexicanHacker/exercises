3e061f451fe99fe3d4b835ade74d5c18a88e9649
Oscar Gonzalez <oscar@mexicanhacker.com>
2009-09-07 20:34:02 -0500

    FizzBuzz Version 2

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,17 +1,25 @@
+# Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the 
+# number and for the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three 
+# and five print “FizzBuzz”.
+
 class FizzBuzz
    def do_it
+         result = ''
         100.times do |i|
             if i%3 == 0         
                 if i%5 == 0
-                    puts "Fizz Buzz"
+                    result << "FizzBuzz\n"
                 else
-                    puts "Fizz"
+                    result << "Fizz\n"
                 end
             elsif i%5 ==0
-                puts "Buzz"
+                result << "Buzz\n"
             else
-                puts "#{i}"
+                result << "#{i}\n"
             end
         end
+        return result
    end
 end
--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -5,7 +5,9 @@ class TestFizzBuzz < Test::Unit::TestCase
    
    def test_multiples_of_three
        fb = FizzBuzz.new
-       fb.do_it
-       #TODO How to test this?
+       result = fb.do_it
+      assert_equal(34, result.scan(/Fizz/).length)
+      assert_equal(20, result.scan(/Buzz/).length)
+      assert_equal(7,result.scan(/FizzBuzz/).length)
    end 
 end
